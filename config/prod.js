7c60f5a6b10db93bb0593364a6124c29ab352269
Ridan77 <dan.ribak17@gmail.com>
2025-08-31 20:50:53 +0300

    setup mongo production

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-  dbURL: process.env.MONGO_URL || 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
-  dbName : process.env.DB_NAME || 'instagram'
+  dbURL: process.env.MONGO_URL ,
+  dbName : process.env.DB_NAME 
 }
