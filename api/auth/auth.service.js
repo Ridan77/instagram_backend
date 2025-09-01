f9f802db7adb436c25bb70b212c8ff0742a46ef4
Ridan77 <dan.ribak17@gmail.com>
2025-09-01 17:39:59 +0300

    new build

--- api/auth/auth.service.js
+++ api/auth/auth.service.js
@@ -18,7 +18,8 @@ async function login(username, password) {
 
 	const user = await userService.getByUsername(username)
 	if (!user) return Promise.reject('Invalid username or password')
-
+		console.log(username,password);
+		
 	// TODO: un-comment for real login
 	const match = await bcrypt.compare(password, user.password)
 	if (!match) return Promise.reject('Invalid username or password')
@@ -61,3 +62,6 @@ function validateToken(loginToken) {
 	}
 	return null
 }
+
+
+
