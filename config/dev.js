edbf7ffb57f6c1dca66c7c14985d55ff6c0f5af6
Ridan77 <dan.ribak17@gmail.com>
2025-09-12 22:45:22 +0300

    new deploy

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,4 @@
 export default  {
-  // dbURL: 'mongodb://127.0.0.1:27017',
-  dbURL: 'mongodb+srv://danribak17:barbur2020@cluster0.kmj1q5t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-
+  dbURL: 'mongodb://127.0.0.1:27017',
   dbName : 'instagram'
 }
